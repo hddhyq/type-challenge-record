37a40b7d1faffe61aae403a712030845139d1e12
hddhyq <brokenbonesdd@gmail.com>
2022-07-05 23:23:58 +0800

    feat: add 268 IF answer

--- simple/index.ts
+++ simple/index.ts
@@ -127,3 +127,14 @@ type MyAwaited<T> = T extends Promise<infer P> ? (P extends Promise<unknown> ? M
 
 // // @ts-expect-error
 // type error = MyAwaited<number>
+
+/** 268 IF */
+type If<C extends boolean, T, F> = C extends true ? T : F
+
+// type cases = [
+//   Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
+//   Expect<Equal<If<false, 'a', 2>, 2>>,
+// ]
+
+// // @ts-expect-error
+// type error = If<null, 'a', 'b'>
