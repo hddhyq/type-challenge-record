55d9dc151991c31fa7b00a7ef38a85bf3526b506
hddhyq <brokenbonesdd@gmail.com>
2022-07-06 23:45:53 +0800

    feat: add 533 concat answer

--- simple/index.ts
+++ simple/index.ts
@@ -138,3 +138,13 @@ type If<C extends boolean, T, F> = C extends true ? T : F
 
 // // @ts-expect-error
 // type error = If<null, 'a', 'b'>
+
+/** 533 - Concat */
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U]
+
+// type cases = [
+//   Expect<Equal<Concat<[], []>, []>>,
+//   Expect<Equal<Concat<[], [1]>, [1]>>,
+//   Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
+//   Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
+// ]
